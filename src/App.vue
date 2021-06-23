605a0684a5cdf0aa1638282a5f6cab69f6933f62
ErSafin <easafin75@gmail.com>
2021-06-23 10:56:24 +0300

    23-06-2021

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     </div>
     <h5 class="primary mx-4">Исполнитель</h5>
     <div id="footer" class="container-fluid mx-2 row">
-        <select id="inputUser" v-model="id" class="form-control col-md-4 mb-1 " autofocus @change="chEntry">
+        <select id="inputUser" v-model="users.id" class="form-control col-md-4 mb-1 " autofocus @change="chEntry(users.id)">
             <option v-for="(value,key) in users" v-bind:key="key" v-bind:value="value.id">
                 {{value.name}}
             </option>
@@ -15,14 +15,14 @@
         <button id="btnEdit" class="btn btn-secondary col-md-1 mb-1 mx-3"
                 @click="setUser">Выбрать</button>
         <button id="btnNew" class="btn btn-secondary col-md-1 mb-1 mx-3"> Новый </button>
-        <button id="btnReturn" class="btn btn-secondary col-md-2 mb-1 mx-3" disabled="true"
+        <button id="btnReturn" class="btn btn-secondary col-md-2 mb-1 mx-3" disabled
                 @click="varUser">Сменить исполнителя</button>
         
     </div>
         <hr />
     <div>
         <h5 class="primary mx-4">Список заявок</h5>
-        <select id="spWork" v-model="work" class="form-control col-md-10 mb-3 mx-4" size="3" @change="viewWork" disabled="true">
+        <select id="spWork" v-model="work" class="form-control col-md-10 mb-3 mx-4" size="3" @change="viewWork" disabled>
             <option v-for="work in outWork" v-bind:key="work" v-bind:value="work">
                 {{work.data}} | {{work.typeApp}} :  {{work.txtApp}}
             </option>
@@ -30,10 +30,10 @@
     </div>
 
     <div class="container-fluid" id="btnD">
-        <button id="btnRed" class="btn btn-secondary col-md-2 mb-1 mx-3" disabled="true"
+        <button id="btnRed" class="btn btn-secondary col-md-2 mb-1 mx-3" disabled
                 @click="editDecl">Редактировать</button>
-        <button id="btnN" class="btn btn-secondary col-md-2 mb-1 mx-3" disabled="true">Новая заявка</button>
-        <button id="btnSave" class="btn btn-secondary col-md-2 mb-1 mx-3" disabled="true"
+        <button id="btnN" class="btn btn-secondary col-md-2 mb-1 mx-3" disabled>Новая заявка</button>
+        <button id="btnSave" class="btn btn-secondary col-md-2 mb-1 mx-3" disabled
                 @click="saveDecl">Сохранить</button>
     </div>
     
@@ -43,18 +43,18 @@
         <br />
         <div class="row">
             <label for="dataDec" class="mx-3">Дата</label>
-            <input type="text" class="form-control col-md-2 mb-1" id="dataDec" value="" required disabled = "true"/>
+            <input type="text" class="form-control col-md-2 mb-1" id="dataDec" value="" required disabled/>
             <label for="typeDec" class="mx-3">Тип заявки</label>
-            <input type="text" class="form-control col-md-6 mb-1" id="typeDec" value="" required disabled = "true"/>
+            <input type="text" class="form-control col-md-6 mb-1" id="typeDec" value="" required disabled/>
         </div>
         <div class="row">
             <label for="statusDec" class="mx-3">Статус</label>
-            <input type="text" class="form-control col-md-2 mb-1" id="statusDec" value="" required disabled = "true"/>
+            <input type="text" class="form-control col-md-2 mb-1" id="statusDec" value="" required disabled/>
             <label for="authorDec" class="mx-3">Автор заявки</label>
-            <input type="text" class="form-control col-md-5 mb-1" id="autDec" value="" required disabled = "true"/>
+            <input type="text" class="form-control col-md-5 mb-1" id="autDec" value="" required disabled/>
         </div>        
         <label for="txtDec">Текст </label>
-        <input type="text" class="form-control col-md-10 mb-1 mx-3" id="txtDec" value="" required disabled = "true"/>
+        <input type="text" class="form-control col-md-10 mb-1 mx-3" id="txtDec" value="" required disabled/>
         <hr />
     </div>
 
@@ -66,33 +66,29 @@
 
 <script>
  // в JSON ввсести поле uniqId - уникальный номер заявки, чтобы по ней можно было сохранять
-
+    
     import spisok from "/App.json"
-   // import accUser from "/menuApp.json"
+    import newuser from "/menuApp.json"
 
  export default {
   name: "App",
   components: {
-
+      
         },
   data() {
       return {
           selected: 1,
-          id: 0,
           outWork: '',
-          work : '',
-          users: [
-              { id: 1, name: 'Иванов И.И.'},
-              { id: 2, name: 'Петров С.П.'},
-              { id: 3, name: 'Квантов А.А.'}
-          ]
+          work: '',
+          users: newuser
+          
       }
   },
   methods: {
-      chEntry() {
+      chEntry(tid) {
           // значение выбранное из выпадающего списка записывается в id и фильтруется JSON
           this.outWork = spisok.
-              filter(work => work.userId === this.id)
+              filter(work => work.userId === parseInt(tid))
       },
       viewWork() {
           document.getElementById('dataDec').value = this.work.data
